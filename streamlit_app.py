a4f154ae81cdcc9ca34e44bbe33d1482b52e2158
Serdar Sevin <48403508+sevinserdar@users.noreply.github.com>
2024-02-28 20:43:03 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,13 @@ st.header("Learn your car price")
 
 # Subheader
 st.subheader("This is a car predictions app")
+
+# Display Images
+
+# import Image from pillow to open images
+from PIL import Image
+img = Image.open("slideshow-564.jpg")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img, width=200)
