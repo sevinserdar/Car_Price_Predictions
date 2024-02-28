4d113a1719aa00cb25a56071e2fac898c91944a3
Serdar Sevin <48403508+sevinserdar@users.noreply.github.com>
2024-02-28 21:51:20 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 # import module
 import streamlit as st
+import joblib
+import pandas as pd
 
 # Title
 st.title("Car Price Predictions")
@@ -12,54 +14,74 @@ st.subheader("This is a car predictions app")
 
 # Display Images
 
-# import Image from pillow to open images
-from PIL import Image
-img = Image.open("slideshow-564.jpg")
-
-# display image using streamlit
-# width is used to set the width of an image
-st.image(img, width=200)
+st.image("slideshow-564.jpg", caption="Kirmizi Araba", width=400)
 
 
-# radio button
-# first argument is the title of the radio button
-# second argument is the options for the radio button
-status = st.radio("Select Color: ", ('White', 'Grey', 'Black', 'Red'))
+st.write("İlgilendiğiniz aracın tahmini piyasa değerini aşağıda görebilirsiniz.")
 
-# conditional statement to print 
-# Male if male is selected else print female
-# show the result using the success function
-if (status == 'White'):
-	st.success("White")
-elif (status == 'Grey'):
-    st.success("Grey")
-elif (status == 'Black'):
-    st.success("Black")
-else:
-	st.success("Red")
+columns = joblib.load("features_list.joblib")
+
+min_year = 2003
+max_year = 2018
+year = st.number_input("Year: ", min_value=2003, max_value=2018)
+
+min_present_price = 0.1
+max_present_price = 100.
+present_price = st.slider("Present_Price", min_value=min_present_price, max_value=max_present_price)
+
+min_km = 500
+max_km = 500_000
+km = st.slider("km", min_value=min_km, max_value=max_km)
+
+fuel_type = st.selectbox(
+    'Fuel Type',
+    ['Petrol', 'Diesel', 'CNG'])
+
+st.write('You selected', fuel_type)
+
+seller = st.selectbox(
+    'Owner',
+    ['Dealer', 'Individual'])
+
+st.write('You selected', seller)
+
+transmission = st.selectbox(
+    'Transmission',
+    ['Manual', 'Automatic'])
+
+st.write('You selected', transmission)
+
+owner = st.selectbox(
+    'Owner',
+    [0, 1, 3])
+
+st.write('You selected', owner)
+
+sample_one = [{
+"Year":year,
+"Present_Price":present_price,
+"Kms_Driven":km,
+"Fuel_Type":fuel_type,
+"Seller_Type":seller,
+"Transmission":transmission,
+"Owner":owner
+    }]
 
 
-# Selection box
+df_s = pd.DataFrame(sample_one)
+st.dataframe(df_s)
 
-# first argument takes the titleof the selectionbox
-# second argument takes options
-hobby = st.selectbox("Year: ",
-					['Jeep', 'SUV', 'Sports'])
+df_s["Year"] = max_year-df_s["Year"]
+df_s = pd.get_dummies(df_s).reindex(columns=columns, fill_value=0)
 
-# print the selected hobby
-st.write("Your hobby is: ", hobby)
+scaler = joblib.load(open("scaler.joblib","rb"))
+model = joblib.load(open("xgb_model.joblib","rb"))
+df_s = scaler.transform(df_s)
+
+if st.button("Make Predict"):
+    pred_price = round(model.predict(df_s)[0] * 10_000)
+    st.write(f"Your car's price: ${pred_price}")
 
 
 
-# Text Input
 
-# save the input text in the variable 'name'
-# first argument shows the title of the text input box
-# second argument displays a default text inside the text input area
-name = st.text_input("Enter Your name", "Type Here ...")
-
-# display the name when the submit button is clicked
-# .title() is used to get the input text string
-if(st.button('Submit')):
-	result = name.title()
-	st.success(result)
