7f5edcf75aed27b3969d94e36bd09150bc7c5ad1
Serdar Sevin <48403508+sevinserdar@users.noreply.github.com>
2024-02-28 20:33:00 +0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+# import module
+import streamlit as st
+
+# Title
+st.title("Car Price Predictions")
+
+# Header
+st.header("Learn your car price") 
+
+# Subheader
+st.subheader("This is a car predictions app")
